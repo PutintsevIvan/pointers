1bdf0d957cc32ab74d429a519b8dcfb16d4f7088
79283 <79283@LAPTOP-TT6N6EBU>
2023-07-25 12:43:16 +0300

    done

--- TransfersParameters/main.cpp
+++ TransfersParameters/main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 #define tab "\t"
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << tab << b << endl;
-	Exchange(a, b);
+	Exchange(&a, &b);
 	cout << a << tab << b << endl;
 }
-void Exchange(int a, int b)
+void Exchange(int* a, int* b)
 {
-	int buffer = a;
-	a = b;
-	b = buffer;
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
 }
